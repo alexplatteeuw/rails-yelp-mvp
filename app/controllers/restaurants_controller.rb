1c6377fe2ad766685f2d5400b6191afd4f7bf426
Alex Platteeuw <aplatteeuw@outlook.fr>
2020-05-19 19:46:35 +0200

    yelp exercise

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,33 @@
+class RestaurantsController < ApplicationController
+  before_action :find_restaurant, only: [:show]
+
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.new(review_params)
+    if @restaurant.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new
+    end
+  end
+
+  def show
+  end
+
+  private
+
+  def find_restaurant
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def review_params
+    params.require(:restaurant).permit(:name, :address, :category)
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+dishoom = { name: "Dishoom", address: "7 Boundary St, London E2 7JE", category: "belgian"}
+pizza_east = { name: "Pizza East", address: "56A Shoreditch High St, London E1 6PQ", category: "italian" }
+petit_jean = { name: "Chez Petit Jean", address: "161 Avenue d'Italie, 75013 Paris", category: "japanese" }
+clauzel = { name: "Le 23 Clauzel", address: "23 Clauzel 75009 Paris", category: "french" }
+blue_monkey = { name: "Blue Monkey", address: "32 rue Frémicourt 75017 Paris", category: "chinese"  }
+
+[dishoom, pizza_east, petit_jean, clauzel, blue_monkey].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts "Finished!"
